7aba2767fb08f4044b4d86b992a5560262c53168
Gabi644 <gabriellucini644@gmail.com>
2025-09-11 15:18:09 -0300

    Boton de encoder

--- esp32-pio-wokwi/src/main.cpp
+++ esp32-pio-wokwi/src/main.cpp
@@ -2,17 +2,17 @@
 
 
 const byte LED = 23;
-const byte BOTON = 2;
+const byte ENC_PUSH = 19;
 const byte POT = 32;
 volatile bool pantalla = true;
-Device device = Device(128, 64, -1, 14, DHT22);
+Device device = Device(128, 64, -1, 33, DHT22);
 
 void setup() {
   
   Serial.begin(9600);
   device.begin();
   pinMode(LED, OUTPUT);
-  pinMode(BOTON, INPUT_PULLUP);
+  pinMode(ENC_PUSH, INPUT_PULLUP);
 }
 
 void loop() {
@@ -20,32 +20,33 @@ void loop() {
   valor = map(valor, 0, 4095, 14.0, 40.0);
   float hum = device.readHum();
   float t = device.readTemp();
-  Serial.println(valor);
-  if (!digitalRead(BOTON)) {
+
+  if (!digitalRead(ENC_PUSH)) {
     pantalla = !pantalla;
   }
   
   device.clear();
   if (pantalla) {
-    
+    char texto[64];
+    sprintf(texto, "Temp: %.2f C", t);
     if (t >= valor) {
-    
-      device.showDisplay("Temperatura", 0, 15);
+      
+      device.showDisplay(texto, 0, 15);
       device.dibujarSol();
       digitalWrite(LED, HIGH);
     }
     else  {
       
-      device.showDisplay("Temperatura", 0, 15);
+      device.showDisplay(texto, 0, 15);
       device.dibujarCheck();
       digitalWrite(LED, LOW);
     }
     }
   else {
-    
-    device.showDisplay("Humedad", 0, 15);
+    char texto[64];
+    sprintf(texto, "Hum: %.2f %", hum);
+    device.showDisplay(texto, 0, 15);
     device.dibujarGota(hum);
-    Serial.println(hum);
     if (hum < 45) {
       
       digitalWrite(LED, HIGH);
